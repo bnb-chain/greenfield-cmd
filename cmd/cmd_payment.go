803835bb47a86124a26f04d5db2e7b13094bed13
flywukong <2229306838@qq.com>
2023-09-06 17:25:06 +0800

    fix: fix get-quota cmd

--- cmd/cmd_payment.go
+++ cmd/cmd_payment.go
@@ -110,7 +110,8 @@ func getQuotaInfo(ctx *cli.Context) error {
 		return toCmdErr(err)
 	}
 
-	fmt.Printf("quota info:\n charged quota:%d \n free quota:%d \n consumed quota:%d \n free consumed quota: %d \n",
+	fmt.Printf("quota info:\n charged quota:%d \n free quota:%d \n consumed charged quota:%d \n consumed free quota: %d \n",
 		quotaInfo.ReadQuotaSize, quotaInfo.SPFreeReadQuotaSize, quotaInfo.ReadConsumedSize, quotaInfo.FreeConsumedSize)
+
 	return nil
 }
